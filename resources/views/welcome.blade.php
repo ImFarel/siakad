95f26aca85a1b9bd329b204f36ce6b935b5635af
ImFarel <sayafarel@gmail.com>
2017-12-19 11:28:37 +0700

    setting package

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Siakad x Laravel
                 </div>
 
                 <div class="links">
